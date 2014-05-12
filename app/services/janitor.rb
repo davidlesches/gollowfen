341315addb0777015f95c9575df1bf0986cc5538
David Lesches <david@lesches.com>
2014-05-12 18:27:26 -0400

    change jantori

--- app/services/janitor.rb
+++ app/services/janitor.rb
@@ -20,4 +20,38 @@ class Janitor
     Favorite.where("unfavorited = ? AND favorited_at < ?", false, Time.zone.now - 3.days)
   end
 
+  def unfavorite fav
+    begin
+      fav.term.user.twitter.destroy_favorite(fav.tweet_id.to_i)
+    rescue Exception => e
+      # Page no longer exists.
+    ensure
+      fav.unfavorited = true
+      fav.save!
+      sleep 0.1
+    end
+  end
+
 end
+
+# Favorite.where("unfavorited = ? AND favorited_at < ?", false, Time.zone.now - 3.days).find_each { |a| unfavorite a }
+
+
+
+#
+# Favorite.where("unfavorited = ? AND favorited_at < ?", false, Time.zone.now - 3.days).last(1000).each { |fav| unfav(fav) }
+#
+# end
+#
+# def unfav fav
+#   begin
+#   fav.term.user.twitter.destroy_favorite(fav.tweet_id.to_i)
+# rescue Exception => e
+#   puts e.message
+# ensure
+#   fav.unfavorited = true
+#   fav.save!
+#   puts "Unfaved: #{fav.screen_name} for #{fav.tweet_id}"
+#   sleep 0.1
+# end
+# end
