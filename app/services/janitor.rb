66eff084cefbc3bc3ea92b52fecf4bc200a0d1f2
David Lesches <david@lesches.com>
2014-08-10 11:38:30 -0400

    last attempt

--- app/services/janitor.rb
+++ app/services/janitor.rb
@@ -1,49 +1,49 @@
-# class Janitor
-#
-#   def self.clean
-#     new()
-#   end
-#
-#   def initialize
-#     cleanup
-#   end
-#
-#   def cleanup
-#     favorites_to_unfavorite.find_each do |fav|
-#       #fav.term.user.twitter.unfavorite(fav.tweet_id)
-#       #fav.unfavorited = true
-#       #fav.save!
-#       unfavorite fav
-#     end
-#   end
-#
-#   def favorites_to_unfavorite
-#     Favorite.where("unfavorited = ? AND favorited_at > ?", false, Time.zone.now - 60.days)
-#   end
-#
-#   def unfavorite fav
-#     puts fav.permalink
-#     thwak fav, 1
-#     thwak fav, 2
-#     thwak fav, 3
-#     fav.unfavorited = true
-#     fav.save!
-#   end
-#
-#   def thwak fav, number
-#     begin
-#       puts "thwak #{number}"
-#       # fav.term.user.twitter.unfavorite( [fav.tweet_id] )
-#       # fav.term.user.unfavoriter.send 'post', "/favorites/destroy.json?id=#{fav.tweet_id}"
-#       fav.term.user.twitter.post "/1.1/favorites/destroy.json", { id: fav.tweet_id.to_i }
-#     rescue Exception => e
-#       # do nothing
-#       puts e.message
-#     end
-#     sleep 1
-#   end
-#
-# end
-#
-# # Favorite.where("unfavorited = ? AND favorited_at < ?", false, Time.zone.now - 3.days).find_each { |a| unfavorite a }
-#
+class Janitor
+
+  def self.clean
+    new()
+  end
+
+  def initialize
+    cleanup
+  end
+
+  def cleanup
+    favorites_to_unfavorite.find_each do |fav|
+      #fav.term.user.twitter.unfavorite(fav.tweet_id.to_i)
+      #fav.term.user.twitter.unfavorite(fav.tweet_id)
+      #fav.unfavorited = true
+      #fav.save!
+      unfavorite fav
+    end
+  end
+
+  def favorites_to_unfavorite
+    Favorite.where("unfavorited = ? AND favorited_at > ?", false, Time.zone.now - 60.days)
+  end
+
+  def unfavorite fav
+    puts fav.permalink
+    user = fav.term.user
+    thwak user, fav, 1
+    thwak user, fav, 2
+    thwak user, fav, 3
+    fav.unfavorited = true
+    fav.save!
+  end
+
+  def thwak user, fav, number
+    begin
+      puts "thwak #{number}"
+      user.twitter.unfavorite(fav.tweet_id.to_i)
+    rescue Exception => e
+      # do nothing
+      puts e.message
+    end
+    sleep 1
+  end
+
+end
+
+# Favorite.where("unfavorited = ? AND favorited_at < ?", false, Time.zone.now - 3.days).find_each { |a| unfavorite a }
+
