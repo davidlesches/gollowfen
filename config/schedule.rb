a293549bb6036157fed5c1e451f860b55562ca4c
David Lesches <david@lesches.com>
2015-08-12 15:58:39 -0400

    Turn it back on

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,47 +1,44 @@
 
 env :PATH, '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
-#
-# every 1.day, at: '8:30 am' do
-#   runner 'Cronbaby.run_favorites'
-# end
-#
-# every 1.day, at: '9:30 am' do
-#   runner 'Cronbaby.run_favorites'
-# end
-#
-# every 1.day, at: '10:30 am' do
-#   runner 'Cronbaby.run_favorites'
-# end
-#
-# every 1.day, at: '11:30 am' do
-#   runner 'Cronbaby.run_favorites'
-# end
-#
-# every 1.day, at: '3:00 pm' do
-#   runner 'Cronbaby.run_favorites'
-# end
-#
-# every 1.day, at: '6:00 pm' do
-#   runner 'Cronbaby.run_favorites'
-# end
-#
-#
-#
-# every 1.day, at: '8:00 am' do
-#   runner 'Cronbaby.import_followers'
-# end
-#
-# every 1.day, at: '4:00 pm' do
-#   runner 'Cronbaby.import_followers'
-# end
-#
-# every 1.day, at: '11:00 pm' do
-#   runner 'Cronbaby.import_followers'
-# end
-#
-#
-#
-#
-# every 1.day, at: '3:00 am' do
-#   runner 'Janitor.clean'
-# end
+
+every 1.day, at: '8:30 am' do
+  runner 'Cronbaby.run_favorites'
+end
+
+every 1.day, at: '9:30 am' do
+  runner 'Cronbaby.run_favorites'
+end
+
+every 1.day, at: '10:30 am' do
+  runner 'Cronbaby.run_favorites'
+end
+
+every 1.day, at: '11:30 am' do
+  runner 'Cronbaby.run_favorites'
+end
+
+every 1.day, at: '3:00 pm' do
+  runner 'Cronbaby.run_favorites'
+end
+
+every 1.day, at: '6:00 pm' do
+  runner 'Cronbaby.run_favorites'
+end
+
+
+every 1.day, at: '8:00 am' do
+  runner 'Cronbaby.import_followers'
+end
+
+every 1.day, at: '4:00 pm' do
+  runner 'Cronbaby.import_followers'
+end
+
+every 1.day, at: '11:00 pm' do
+  runner 'Cronbaby.import_followers'
+end
+
+
+every 1.day, at: '3:00 am' do
+  runner 'Janitor.clean'
+end
